3d52fa60a7b6e30031d30f93d47864a13445226f
Symon Lima <symonlima07@gmail.com>
2024-11-27 17:25:37 -0300

    Correção de refatoração

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-list',
@@ -11,7 +12,7 @@ import { ProductsService } from '../../shared/services/products.service';
 })
 export class ListComponent {
 
-  products: any[] = [];
+  products: Product[] = [];
 
   productsService = inject(ProductsService);
 
