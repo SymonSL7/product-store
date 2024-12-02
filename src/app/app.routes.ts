54a370a840bbe6f6658885787fa6a001b2a6e671
Symon Lima <symonlima07@gmail.com>
2024-12-02 13:18:23 -0300

    Carrega create-component via lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { CreateComponent } from './features/create/create.component';
 
 export const routes: Routes = [
     {
-    
-    path: '',
-    component: ListComponent
 
-},
-{
-    path: 'create-product',
-    component: CreateComponent,
-}
+        path: '',
+        component: ListComponent
+
+    },
+    {
+        path: 'create-product',
+        loadComponent: () =>
+            import('./features/create/create.component').then(
+                (m) => m.CreateComponent
+            ),
+    }
 ];
