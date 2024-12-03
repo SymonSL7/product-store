87417654ceee854499af38ea8d517ea2b6a88c7f
Symon Lima <symonlima07@gmail.com>
2024-12-03 15:04:43 -0300

    Adiciona mensagem de feedback ao criar produto

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -3,7 +3,9 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { ProductsService } from '../../shared/services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -16,6 +18,10 @@ export class CreateComponent {
 
   productsService = inject(ProductsService);
 
+  matSnackBar = inject(MatSnackBar);
+
+  router = inject(Router);
+  
   form = new FormGroup({
 
     title: new FormControl<string>('', {
@@ -30,9 +36,19 @@ export class CreateComponent {
     this.productsService.post({
       title: this.form.controls.title.value
     })
-    .subscribe(() => {
-      alert("Cadastrado com Sucesso!");
-    });
+      .subscribe(() => {
+
+        this.matSnackBar.open("Produto Cadastrado com Sucesso!", "OK", {
+
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top'
+
+        });
+
+        this.router.navigateByUrl('/');
+
+      });
 
   }
 
