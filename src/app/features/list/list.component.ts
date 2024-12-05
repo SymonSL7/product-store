755fd937ef43dcddeb045cd2ff5d7dbbb857af54
Symon Lima <symonlima07@gmail.com>
2024-12-05 16:00:08 -0300

    Mostrando componente quando não há items

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -6,18 +6,21 @@ import { CardComponent } from './components/card/card.component';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs/internal/operators/filter';
 import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
+import { NoItemsComponent } from './components/no-items/no-items.component';
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule],
+  imports: [CardComponent, RouterLink, MatButtonModule, NoItemsComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
 
-  products = signal<Product[]>(inject(ActivatedRoute).snapshot.data['products']);
+  products = signal<Product[]>(
+    inject(ActivatedRoute).snapshot.data['products']    
+  );
 
   productsService = inject(ProductsService);
 
