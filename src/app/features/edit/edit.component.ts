2f4f4faf9b5ef46630bfa1a252371f66ba4c386b
Symon Lima <symonlima07@gmail.com>
2024-12-05 16:55:03 -0300

    Criando link para voltar para página inicial, nos formulários

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Product } from '../../shared/interfaces/product.interface';
 import { FormComponent } from '../../shared/components/form/form.component';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToListComponent],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
